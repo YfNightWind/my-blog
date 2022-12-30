92ebbe9196792a30248eb0392d5c00e5299d8158
YfNightWind <linyuhong.personal@outlook.com>
2022-12-30 20:21:59 +0800

    feat: 添加错误信息

--- utils/errormsg/error_message.go
+++ utils/errormsg/error_message.go
@@ -13,22 +13,28 @@ const (
 	ERROR_TOKEN_TYPE_WRONG = 1007
 
 	// code = 200...文章模块的错误
+	ERROR_ARTICLE_NOT_EXIST = 2001
 
 	// code = 300...分类模块的错误
-	ERROR_CATEGORYNAME_USED = 3001
+	ERROR_CATEGORYNAME_USED  = 3001
+	ERROR_CATEGORY_NOT_EXIST = 3002
 )
 
 var codeMsg = map[int]string{
-	SUCCESS:                 "成功！",
-	ERROR:                   "错误",
-	ERROR_USERNAME_USED:     "用户名已被使用",
-	ERROR_PASSWORD_WRONG:    "用户密码错误",
-	ERROR_USER_NOT_EXIST:    "该用户不存在",
-	ERROR_TOKEN_NOT_EXIST:   "token不存在",
-	ERROR_TOKEN_RUNTIME:     "token过期",
-	ERROR_TOKEN_WRONG:       "token错误",
-	ERROR_TOKEN_TYPE_WRONG:  "token格式错误",
-	ERROR_CATEGORYNAME_USED: "该分类已存在",
+	SUCCESS:                "成功！",
+	ERROR:                  "错误",
+	ERROR_USERNAME_USED:    "用户名已被使用",
+	ERROR_PASSWORD_WRONG:   "用户密码错误",
+	ERROR_USER_NOT_EXIST:   "该用户不存在",
+	ERROR_TOKEN_NOT_EXIST:  "token不存在",
+	ERROR_TOKEN_RUNTIME:    "token过期",
+	ERROR_TOKEN_WRONG:      "token错误",
+	ERROR_TOKEN_TYPE_WRONG: "token格式错误",
+
+	ERROR_CATEGORYNAME_USED:  "该分类已存在",
+	ERROR_CATEGORY_NOT_EXIST: "该分类不存在",
+
+	ERROR_ARTICLE_NOT_EXIST: "该文章不存在",
 }
 
 func GetErrorMsg(code int) string {
